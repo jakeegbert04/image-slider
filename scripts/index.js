f315d48bb026e1a1e64fe95ad69cb49f50091976
Jake Egbert <jake.egbert04@gmail.com>
2023-02-16 14:03:12 -0700

    Image Slider

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const slider = document.getElementById("slider");
+const foregroundImage = document.querySelector(".foreground-img");
+
+slider.addEventListener("input", e => {
+  const sliderValue = e.target.value;
+
+  foregroundImage.style.width = `${sliderValue}%`;
+  console.log(sliderValue);
+});
